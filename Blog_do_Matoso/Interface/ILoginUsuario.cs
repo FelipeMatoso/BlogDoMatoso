a7d3c08648a2a2813c5c9db10ff2644bc9118930
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2021-01-14 18:06:05 -0300

    Create ILoginUsuario.cs

---
+++ Blog_do_Matoso/Interface/ILoginUsuario.cs
@@ -0,0 +1,13 @@
+﻿using Blog_do_Matoso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_do_Matoso.Interface
+{
+    public interface ILoginUsuario
+    {
+        string ValidacaoUsuarioLogin(string nome , string senha);
+    }
+}
