3114d21f0ff62705b5e0c37228a2110eaaed4220
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2021-01-14 16:30:18 -0300

    -addfile C# "adicionado para validação de login e cadastro de usuario
    Inicio da implementação da camada Business

---
+++ Blog_do_Matoso/Business/ValidacaoLoginUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_do_Matoso.Business
+{
+    public class ValidacaoLoginUsuario
+    {
+    }
+}
