6e2cd95bb950b5cfe05389551e991563a3410131
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2021-01-15 16:26:02 -0300

    -func "Implementado metodos com acesso a bd"

---
+++ Blog_do_Matoso/Repositorio/LoginUsuario.cs
@@ -0,0 +1,27 @@
+﻿using Blog_do_Matoso.Interface;
+using Blog_do_Matoso.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_do_Matoso.Business
+{
+    public class LoginUsuario : ILoginUsuario
+    {
+        DBcontext dBcontext;
+
+        public LoginUsuario(DBcontext dBcontext)
+        {
+            this.dBcontext=dBcontext;
+        }
+
+        public bool ValidacaoUsuarioLogin(string nome , string senha)
+        {
+
+            //valida se o login é login aceito
+            return true;
+        }
+    }
+}
