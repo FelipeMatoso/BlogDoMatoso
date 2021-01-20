be1c8c96e5f5ad458f3dcd07c1a6ef7644259d48
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2021-01-20 13:36:22 -0300

    Create AutorizaComponentesLogin.js

---
+++ Blog_do_Matoso/wwwroot/js/AutorizaComponentesLogin.js
@@ -0,0 +1,18 @@
+﻿function checaLogin() {
+    console.log("entro")
+    let algo = localStorage.getItem("nomeUsuario");
+    $("#IconeUsuario").text(`${algo}`)
+    EscondeCaixasDeLogin();
+
+}
+
+function EscondeCaixasDeLogin() {
+    if (localStorage.getItem("nomeUsuario") != null) {
+        $("#CaixaLogin").attr("hidden", true);
+        $("#BoxInsereDepoimento").attr("hidden", false);
+    }
+    else {
+        $("#BoxInsereDepoimento").attr("hidden", true);
+        $("#CaixaUserLogado").attr("hidden", true);
+    }
+}
