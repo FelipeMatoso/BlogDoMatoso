9cd6a206a97df6b5ff81d1a95e464f9c03042645
FelipeMatoso <73952306+FelipeMatoso@users.noreply.github.com>
2021-01-25 15:14:12 -0300

    Create Depoimentos_LikesEApagar.js

---
+++ Blog_do_Matoso/wwwroot/js/Depoimentos_LikesEApagar.js
@@ -0,0 +1,40 @@
+﻿function apagaDepoimentoDB(btnApagar) {
+    console.log("apagou")
+
+    console.log(btnApagar)
+    console.log(btnApagar.dataset.id)
+
+    AjaxApagaDepoimento( typeof btnApagar.dataset.id)
+
+}
+
+function AjaxApagaDepoimento(id) {
+    let idDepoimento = { idDepoiemento: id };
+    $.ajax({
+        method: "POST",
+        url: "/home/ApagaDepoimentoDB",
+        headers: 'json',
+        data: idDepoimento,
+        async: true,
+        beforeSend: function () {
+            console.log("Apagando depoimento...");
+        },
+        success: function (response) {
+            console.log(response)
+            console.log("Depoimento de id" + id + ", de " + localStorage.getItem("idUsuario") + "apagado ")
+        },
+        error: function (req, status, err) {
+            console.log("deu error")
+            console.log(req)
+            console.log(status)
+            console.log(err)
+
+
+        }
+    })
+}
+
+function likeDepoimentoDB(btnLike) {
+    console.log("like")
+    return true;
+}
